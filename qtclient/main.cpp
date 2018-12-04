696add57daefbd3f83d661065740f969614b2922
Jacob Gladish <jacobgladish@yahoo.com>
2018-12-04 12:55:52 -0500

    Added ability to run tests

--- qtclient/main.cpp
+++ qtclient/main.cpp
@@ -58,6 +58,38 @@ QJsonDocument test_getWiFiStatus()
   return doc;
 }
 
+QJsonDocument getTest(QCommandLineParser& parser, QMap< QString, std::function<QJsonDocument()> >& testCases)
+{
+  QJsonDocument jsonRequest;
+
+  if (parser.isSet("request"))
+  {
+    QString req = parser.value("request");
+    if (!fileExists(req))
+    {
+      std::cerr << "cannot find: " << qPrintable(req) << std::endl;
+      return jsonRequest;
+    }
+
+    jsonRequest = jsonFromFile(req);
+  }
+  else if (parser.isSet("test"))
+  {
+    QString testCase = parser.value("test");
+    auto iter = testCases.find(testCase);
+    if (iter == testCases.end())
+    {
+      std::cerr << "test case:" << qPrintable(testCase) << " not found" << 
+        std::endl;
+      return jsonRequest;
+    }
+
+    jsonRequest = iter.value()();
+  }
+
+  return jsonRequest;
+}
+
 void
 requires(QString const& arg)
 {
@@ -69,13 +101,18 @@ int main(int argc, char* argv[])
 {
   bool debug = false;
 
-  QJsonDocument     jsonRequest = test_getWiFiStatus();
+  QJsonDocument jsonRequest;
   QBluetoothAddress target;
   QBluetoothAddress adapter;
 
   QCoreApplication app(argc, argv);
   QCoreApplication::setApplicationName("BLE RPC Test Client");
   QCoreApplication::setApplicationVersion("1.0");
+
+  QMap< QString, std::function<QJsonDocument ()> > testCases
+  {
+    {"get-wifi-status", test_getWiFiStatus}
+  };
 
   parser.setApplicationDescription("Test BLE interface on RDKC xCam2");
   parser.addHelpOption();
@@ -85,40 +122,49 @@ int main(int argc, char* argv[])
     {{"t", "target"}, "The BLE MAC address for the target device", "mac"},
     {{"a", "ble-adapter"}, "For computers with multiple BLE adapters, use to set mac", "mac"},
     {{"r", "request"}, "The RPC call to make. This can be path to JSON or actual json string"},
+    {{"l", "list"}, "List available test cases"},
+    {{"z", "test"}, "Run a given test", "test"}
   });
   parser.process(app);
+
+  if (parser.isSet("list"))
+  {
+    std::cout << std::endl;
+    std::cout << "Available test cases" << std::endl;
+    for (QString const& s : testCases.keys())
+      std::cout << "\t" << qPrintable(s) << std::endl;
+    std::cout << std::endl;
+    return 0;
+  }
 
   if (parser.isSet("debug"))
     debug = true;
 
-  // allow for tesing
-  if (jsonRequest.isEmpty())
+  if (!parser.isSet("test"))
   {
-    if (!parser.isSet("request"))
-    {
-      std::cerr << "operation reqired" << std::endl;
-      return 0;
-    }
-
-
-    QString req = parser.value("request");
-    if (!fileExists(req))
-    {
-      std::cerr << "cannot find: " << qPrintable(req) << std::endl;
-      return 0;
-    }
-
-    jsonRequest = jsonFromFile(req);
+    std::cout << "please provide test case with --test" << std::endl;
+    return 0;
   }
 
-  BluetoothApplication bleapp(adapter);
+  jsonRequest = getTest(parser, testCases);
+  if (jsonRequest.isEmpty())
+  {
+    std::cerr << "invalid json request" << std::endl;
+    return 0;
+  }
 
   if (debug)
     QLoggingCategory::setFilterRules(QStringLiteral("qt.bluetooth* = true"));
 
   target = QBluetoothAddress(parser.value("target"));
-  bleapp.run(target, jsonRequest);
+  if (target.isNull())
+  {
+    std::cerr << "invalid bluetooth address:" << qPrintable(parser.value("target"))
+      << std::endl;
+    return 0;
+  }
 
+  BluetoothApplication bleapp(adapter);
+  bleapp.run(target, jsonRequest);
   return app.exec();
 }
-
