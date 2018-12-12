f5b2e7caa7e32fd31d7e75a609881d45fb274564
Gladish <jgladi200@cable.comcast.com>
2018-12-12 15:34:58 -0500

    Updated license header

--- bluez/beacon.h
+++ bluez/beacon.h
@@ -1,5 +1,5 @@
 //
-// Copyright [2018] [jacobgladish@yahoo.com]
+// Copyright [2018] [Comcast NBCUniversal]
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -13,7 +13,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 //
-
 #ifndef __BEACON_H__
 #define __BEACON_H__
 
